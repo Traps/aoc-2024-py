e550ed94d889cc7322dcb8e606010c2b617498ff
Olof Gustavsson <org@Arcus.localdomain>
2025-01-10 16:56:22 +0100

    some cleanup, simplification and de-bloating

--- src/aoc2024/solutions/day17b.py
+++ src/aoc2024/solutions/day17b.py
@@ -1,5 +1,4 @@
-from types import FunctionType
-from typing import Generator, Iterable
+from typing import Callable, Generator, Iterable
 
 from .day17a import parse_setup
 
@@ -15,8 +14,7 @@ def stringify_instruction(opcode:int, operand:int) -> tuple[str, str]:
         case 2: # bst
             return ('b', f'{combo_val(operand)} & {0b111}')
         case 3: # jnz
-            assert operand == 0
-            return ('RECURSE', f'{{FUNCTION_NAME}}(outputs, a)')
+            return ('RECURSE', f'a')
         case 4: # bxc
             return ('b', 'b ^ c')
         case 5: # out
@@ -27,7 +25,8 @@ def stringify_instruction(opcode:int, operand:int) -> tuple[str, str]:
             return ('c', f'a >> {combo_val(operand)}')
 
 
-def reduce_program(instructions:list[tuple[str, str]]) -> str:
+def reduce_program(instructions:list[tuple[str, str]]
+                   ) -> tuple[Callable[[int], int], Callable[[int], int]]:
     var_exprs = {'a': 'a'}
 
     for instruction in instructions:
@@ -39,26 +38,16 @@ def reduce_program(instructions:list[tuple[str, str]]) -> str:
         var_exprs[var] = expr
     
     output_expr = var_exprs['OUTPUT']
-    recursive_call = var_exprs['RECURSE'].replace('>>', '<<')
+    recurse_expr = var_exprs['RECURSE'].replace('>>', '<<')
 
-    return (
-        f'def {{FUNCTION_NAME}}(outputs, _a):\n'
-        f'    *outputs, next_output = outputs\n'
+    f_output = eval(f'lambda a: {output_expr}')
+    f_recurse = eval(f'lambda a: {recurse_expr}')
 
-        f'    for a in range(_a, _a + 8):\n'
-        f'        if {output_expr} != next_output:\n'
-        f'            continue\n'
-        
-        f'        if outputs:\n'
-        f'            yield from {recursive_call}\n'
-        f'        else:\n'
-        f'            yield a\n'
-    )
+    return f_output, f_recurse
 
 
-def find_self_replicating_values(program:Iterable[int]) -> Generator[int, None, None]:
-    GLOBAL_FUNCTION_ALIAS:str = 'reversed_chronospatial_program'
-
+def find_self_replicating_values(program:Iterable[int]
+                                 ) -> Generator[int, None, None]:
     program = tuple(program)
     
     assert sum(op == 3 for op in program[::2]) == 1
@@ -68,57 +57,25 @@ def find_self_replicating_values(program:Iterable[int]) -> Generator[int, None, 
     for opcode,operand in zip(program[0::2], program[1::2]):
         exprs.append(stringify_instruction(opcode, operand))
 
-    rev_def = reduce_program(exprs).format(FUNCTION_NAME=GLOBAL_FUNCTION_ALIAS)
+    f_output, f_recurse = reduce_program(exprs)
 
-    rev_code = compile(rev_def, "<string>", "exec")
+    def reversed_program(outputs:tuple[int, ...],
+                         _a:int=0) -> Generator[int, None, None]:
+        *outputs, next_output = outputs
 
-    _globals = globals()
+        for a in range(_a, _a + 8):
+            if f_output(a) != next_output:
+                continue
+                
+            if outputs:
+                yield from reversed_program(outputs, f_recurse(a))
+            else:
+                yield a
 
-    rev_prog = FunctionType(rev_code.co_consts[0], _globals, GLOBAL_FUNCTION_ALIAS)
-    
-    _globals[GLOBAL_FUNCTION_ALIAS] = rev_prog
-    
-    yield from rev_prog(program, 0)
-
-    _globals.pop(GLOBAL_FUNCTION_ALIAS)
-    
-        
-
-
-
-# def reverse_program(outputs:list[int], a:int=0) -> Generator[int, None, None]:
-#     if len(outputs) == 0:
-#         yield a
-#         return
-    
-#     next_out = outputs[-1]
-
-#     for b in range(8):
-#         _a = (a << 3) | b
-
-#         if b ^ 0b110 ^ (_a >> (b ^ 0b011)) & 0b111 == next_out:
-#             yield from reverse_program(outputs[:-1], _a)
-
+    yield from reversed_program(program)
 
 
 def solve(_intput:str) -> str:
-    registers, program = parse_setup(_intput)
-
-    
+    _, program = parse_setup(_intput)
 
     return min(find_self_replicating_values(program))
-    
-
-    # return list(rev_prog(*registers)) #min(reverse_program(program))
-
-
-
-
-
-
-
-
-
-
-
-
